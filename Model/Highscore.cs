a95d469164af07cf6c09b25bc8c4a8e3fed4cf99
asurr876 <asurr876@students.bju.edu>
2018-04-02 17:54:48 -0700

    View and gitignore

---
+++ Model/Highscore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twisted_Treeline.View;
+using Twisted_Treeline;
+using System.IO;
+
+namespace Twisted_Treeline.Model
+{
+    class Highscore
+    {
+        public StreamWriter writer = new StreamWriter("HighScoreList.txt");
+        public StreamReader reader = new StreamReader("HighScoreList.txt");
+
+        public int Score;
+        public string Name;
+
+        public Highscore(string nickname, int score)
+        {
+           Name = nickname;
+           Score = score;
+           AddToList(this);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="score"></param>
+        /// <returns>index</returns>
+        public int GetRank(int score)
+        {
+            return 0;
+        }
+        public void AddToList(Highscore highscore)
+        {
+            using (reader)
+            {
+                int score = 999999999;
+                int rank = 0;
+                while(score > highscore.Score)
+                {
+                        //read rank
+                        reader.ReadLine();
+                        //read score
+                        score = Convert.ToInt32(reader.ReadLine());
+                        //read nickname
+                        reader.ReadLine();
+
+                    
+                }
+            }
+            using (writer)
+            {
+                writer.WriteLine("highscore.Rank.ToString()" + "\n" + highscore.Score.ToString() + "\n" + highscore.Name + "\n");
+            }
+        }
+    }
+}
---
+++ View/HighscorePrompt.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Twisted_Treeline.Model;
+using System.IO;
+
+namespace Twisted_Treeline.View
+{
+    /// <summary>
+    /// Interaction logic for HighscorePrompt.xaml
+    /// </summary>
+    public partial class HighscorePrompt : Window
+    {
+        public HighscorePrompt()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string name = Name.Text;
+            new Highscore(name).AddToList();
+
+        }
+    }
+}
