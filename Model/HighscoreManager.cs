a0baf5560782ee2f12a83613a5476284b8ac63cd
IamSurrett <amy@surrett.org>
2018-04-08 21:30:00 -0700

    highscores working!!!

--- Model/HighscoreManager.cs
+++ Model/HighscoreManager.cs
@@ -10,15 +10,14 @@ namespace Twisted_Treeline.Model
     class HighscoreManager
     {
         public List<Highscore> HighscoreList;
-        public string Filename;
-        StreamWriter Writer;
+        public string Filename { get; set; }
 
         public HighscoreManager(string filename)
         {
             HighscoreList = new List<Highscore>();
             Filename = filename;
-            FileStream file = new FileStream(filename, FileMode.Open ,FileAccess.Write);
-            StreamWriter Writer = new StreamWriter(file);
+            //FileStream file = new FileStream(filename, FileMode.Open ,FileAccess.Write);
+            //StreamWriter Writer = new StreamWriter(file);
         }
 
         /// <summary>
@@ -26,8 +25,8 @@ namespace Twisted_Treeline.Model
         /// </summary>
         public void LoadList()
         {
-            using(FileStream rfstream = new FileStream(Filename, FileMode.Open, FileAccess.Read)) { 
-            using (StreamReader reader = new StreamReader(rfstream))
+            //using(FileStream rfstream = new FileStream(Filename, FileMode.Open, FileAccess.Read)) { 
+            using (StreamReader reader = new StreamReader(Filename))
             {
                     while (!reader.EndOfStream)
                     {
@@ -36,16 +35,22 @@ namespace Twisted_Treeline.Model
                         this.HighscoreList.Add(new Highscore(Convert.ToInt32(scoreArray[0]), scoreArray[1]));
                     }
                 }
-            }
+            //}
         }
         /// <summary>
         /// Updates the txt file to reflect scores and names in `HighscoreList`
         /// </summary>
         public void SaveList(Highscore hscore)
         {
-            Writer.WriteLine(hscore.Score + "," + hscore.Name);
-            Writer.Flush();
-            Writer.Dispose();
+            using(StreamWriter writer = new StreamWriter(Filename))
+            {
+            foreach (Highscore highscore in this.HighscoreList)
+                {
+                    writer.WriteLine(highscore.Score + "," + highscore.Name);
+                }
+                writer.WriteLine(hscore.Score + "," + hscore.Name);
+                writer.Flush();
+            }
         }
     }
 }
