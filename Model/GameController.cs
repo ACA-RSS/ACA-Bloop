727f27cc802c4f0f8d0c83a11a9e2adcde1be78b
casmith98 <csmit757@students.bju.edu>
2018-04-21 21:55:02 -0400

    ghjk

--- Model/GameController.cs
+++ Model/GameController.cs
@@ -126,7 +126,7 @@ namespace Twisted_Treeline.Model
             Star glitter = new Star() { Spot = new Location { Row = 5, Column = 5 } };
             Star gleam = new Star() { Spot = new Location { Row = 13, Column = 5 } };
             Star glow = new Star() { Spot = new Location { Row = 10, Column = 17 } };
-            
+
             Instance.Level.WorldObj.Add(glitter);
             Instance.Level.WorldObj.Add(gleam);
             Instance.Level.WorldObj.Add(glow);
@@ -279,7 +279,8 @@ namespace Twisted_Treeline.Model
 
         }
 
-        public void SetUpLevelThreePtTwo() {
+        public void SetUpLevelThreePtTwo()
+        {
             Instance.Level = new World()
             {
                 Height = 24,
